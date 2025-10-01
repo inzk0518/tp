d95ddbe17cb160a72213f843d4edbec0b6deef6a
joojaja <194677730+joojaja@users.noreply.github.com>
2025-10-01 23:25:27 +0800

    Change keyword to addcontact

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -19,7 +19,7 @@ import seedu.address.model.person.Uuid;
  */
 public class AddCommand extends Command {
 
-    public static final String COMMAND_WORD = "add";
+    public static final String COMMAND_WORD = "addcontact";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
             + "Parameters: "
