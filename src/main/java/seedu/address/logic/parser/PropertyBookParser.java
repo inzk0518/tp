4f73fff64f20a9370cc6b3e6068bfeec21d8b6a8
joojaja <194677730+joojaja@users.noreply.github.com>
2025-10-08 00:30:24 +0800

    Add new commands to parser

--- src/main/java/seedu/address/logic/parser/PropertyBookParser.java
+++ src/main/java/seedu/address/logic/parser/PropertyBookParser.java
@@ -11,6 +11,8 @@ import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.AddPropertyCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.MarkSoldCommand;
+import seedu.address.logic.commands.MarkUnsoldCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.logic.parser.exceptions.UnknownCommandParseException;
 
@@ -50,6 +52,10 @@ public class PropertyBookParser implements CommandSetParser {
         switch (commandWord) {
         case AddPropertyCommand.COMMAND_WORD:
             return new AddPropertyCommandParser().parse(arguments);
+        case MarkSoldCommand.COMMAND_WORD:
+            return new MarkSoldCommandParser().parse(arguments);
+        case MarkUnsoldCommand.COMMAND_WORD:
+            return new MarkUnsoldCommandParser().parse(arguments);
         default:
             throw new UnknownCommandParseException(MESSAGE_UNKNOWN_COMMAND);
         }
