108cfd938aa76524663bebea9c5a61457f6d6c29
joojaja <194677730+joojaja@users.noreply.github.com>
2025-10-08 21:43:31 +0800

    Fix merge conflict

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -15,8 +15,8 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.PersonAddress;
-import seedu.address.model.person.Phone;
 import seedu.address.model.person.PersonStatus;
+import seedu.address.model.person.Phone;
 
 public class JsonAdaptedPersonTest {
     private static final String INVALID_NAME = "R@chel";
