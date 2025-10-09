3b3795de7706557d08b7351ba9f4d40b22da6267
joojaja <194677730+joojaja@users.noreply.github.com>
2025-10-09 18:47:53 +0800

    Fix checkstyle

--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -14,7 +14,6 @@ import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
 import seedu.address.model.property.Property;
-import seedu.address.model.property.Status;
 
 /**
  * Represents the in-memory model of the address book data.
--- src/test/java/seedu/address/model/ModelManagerTest.java
+++ src/test/java/seedu/address/model/ModelManagerTest.java
@@ -2,7 +2,6 @@ package seedu.address.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
