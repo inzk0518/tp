bd623211f3fac49f76afb72fb48e80c3f8785910
mnjp <57860558+MNJPeng@users.noreply.github.com>
2025-10-16 23:12:01 +0800

    Revert "Add property list GUI panel to view properties' details"

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -9,7 +9,6 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.person.Person;
-import seedu.address.model.property.Property;
 
 /**
  * API of the Logic component
@@ -33,9 +32,6 @@ public interface Logic {
 
     /** Returns an unmodifiable view of the filtered list of persons */
     ObservableList<Person> getFilteredPersonList();
-
-    /** Returns an unmodifiable view of the filtered list of properties */
-    ObservableList<Property> getFilteredPropertyList();
 
     /**
      * Returns the user prefs' address book file path.
