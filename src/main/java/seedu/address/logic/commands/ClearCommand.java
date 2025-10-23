e0886a537644d310be3b25460e0a0322fad724ef
Speedoxx <maximusngjp@gmail.com>
2025-10-23 13:50:01 +0800

    Update clear command to clear propertybook

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -4,20 +4,22 @@ import static java.util.Objects.requireNonNull;
 
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
+import seedu.address.model.PropertyBook;
 
 /**
- * Clears the address book.
+ * Clears the address book and property book.
  */
 public class ClearCommand extends Command {
 
     public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String MESSAGE_SUCCESS = "Address book & Property book have been cleared!";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.setAddressBook(new AddressBook());
+        model.setPropertyBook(new PropertyBook());
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
