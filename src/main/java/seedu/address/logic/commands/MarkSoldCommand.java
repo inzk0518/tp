e9e9281fab1b56cc85f3985742563b3683751563
joojaja <194677730+joojaja@users.noreply.github.com>
2025-10-29 17:22:39 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/MarkSoldCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkSoldCommand.java
@@ -53,7 +53,7 @@ public class MarkSoldCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-      
+
         String invalidIdsMessage = getInvalidPropertyIdsMessage(model, propertyIds);
         if (invalidIdsMessage != null) {
             throw new CommandException(invalidIdsMessage);
